d023ebb56ac13bc80cff2f3828165586d24d10a9
Kelly Pauls <kelly.r.pauls@gmail.com>
2023-08-04 10:06:15 -0400

    adding notation to code

--- script.js
+++ script.js
@@ -5,32 +5,40 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 
 function search(str) {
 	let results = [];
+	// turning fruits to lowercase to compare with string
 	const lowercaseFruits = fruit.map(item => item.toLowerCase());
 	for(let i = 0; i < lowercaseFruits.length; i++){
 		if(lowercaseFruits[i].includes(str.toLowerCase())){
 			results.push(lowercaseFruits[i])
 		}
 	}
+	// if fruit includes the string typed into the input, push that into results array
 	return results;
 }
 
 function searchHandler(e) {
+	// selecting text input and plugging that into search function
 	const srcText = e.target.value.toLowerCase();
 	const srcResults = search(srcText);
+	// taking these two variables and using them in showSuggestions function
 	showSuggestions(srcResults, srcText);
 }
 
 function showSuggestions(results, inputVal) {
+	// map over results form search function, replace the letters that are input with bold letters to highlight them in the suggestions list
 	const items = results.map((val) => {
 		const bold = val.replace(inputVal, inputVal.bold());
 		return "<li>" + bold + "</li>";
 	});
+	// adding the items to the ul dropdown menu
 	suggestions.innerHTML = "<ul>" + items.join('') + "</ul>"
 }
 
 function useSuggestion(e) {
+	// if we select a value that has an LI tag, make the input value the inner text of that LI
 	if (e.target.tagName = "LI"){
 		input.value = e.target.innerText;
+		// when selected, remove the suggestion from the list and just have the input with the selected fruit
 		while(suggestions.firstChild){
 			suggestions.removeChild(suggestions.firstChild);
 		}
